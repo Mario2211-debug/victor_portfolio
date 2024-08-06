5c8f0bd7ed7c4bd2a462a2d24442f4ad815253ed
Mario2211-debug <marioafonso1997@gmail.com>
2024-08-06 09:43:05 +0100

    read_me

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -58,61 +58,10 @@ export default function Blog() {
   return (
     <>
       <div className="layout-content-container flex flex-col max-w-[960px] flex-1">
-        {last && (
-          <div className="container">
-            <div className="flex flex-col gap-6 px-4 py-10 md:gap-8 lg:flex-row">
-              <div
-                className="w-full bg-center bg-no-repeat aspect-video bg-cover rounded-xl md:h-auto md:min-w-[400px] lg:w-full"
-                style={{
-                  backgroundImage: `url("${last.imageUrl}")`,
-                }}
-              ></div>
-              <div className="flex flex-col gap-6 @[480px]:min-w-[400px] @[480px]:gap-8 @[864px]:justify-center">
-                <div className="flex flex-col gap-2 text-left">
-                  <h1 className=" text-4xl font-black leading-tight tracking-[-0.033em] @[480px]:text-5xl @[480px]:font-black @[480px]:leading-tight @[480px]:tracking-[-0.033em]">
-                    Explore the World of Development
-                  </h1>
-                  <h2 className=" text-sm font-normal leading-normal @[480px]:text-base @[480px]:font-normal @[480px]:leading-normal">
-                    Let´s dive into the latest trends and insights in the tech
-                    industry.
-                  </h2>
-                </div>
-                <label className="flex flex-col min-w-40 h-14 w-full max-w-[480px] @[480px]:h-16">
-                  <div className="flex w-full flex-1 items-stretch rounded-xl h-full">
-                    <div
-                      className="flex border-none second-element items-center justify-center pl-4 rounded-l-xl border-r-0"
-                      data-icon="MagnifyingGlass"
-                      data-size="20px"
-                      data-weight="regular"
-                    >
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="20px"
-                        height="20px"
-                        fill="currentColor"
-                        viewBox="0 0 256 256"
-                      >
-                        <path d="M229.66,218.34l-50.07-50.06a88.11,88.11,0,1,0-11.31,11.31l50.06,50.07a8,8,0,0,0,11.32-11.32ZM40,112a72,72,0,1,1,72,72A72.08,72.08,0,0,1,40,112Z"></path>
-                      </svg>
-                    </div>
-                    <input
-                      placeholder="Search blog posts"
-                      className="form-input second-element flex w-full min-w-0 flex-1 resize-none overflow-hidden rounded-xl  focus:outline-0 focus:ring-0 border-none focus:border-none h-full placeholder:text-[#999999] px-4 rounded-r-none border-r-0 pr-2 rounded-l-none border-l-0 pl-2 text-sm font-normal leading-normal @[480px]:text-base @[480px]:font-normal @[480px]:leading-normal"
-                      value=""
-                    />
-                    <div className="flex items-center second-element justify-center rounded-r-xl border-l-0 border-none pr-2">
-                      <button className="flex min-w-[84px] max-w-[480px] cursor-pointer items-center justify-center overflow-hidden rounded-full h-10 px-4 @[480px]:h-12 @[480px]:px-5 bg-[#019863]  text-sm font-bold leading-normal tracking-[0.015em] @[480px]:text-base @[480px]:font-bold @[480px]:leading-normal @[480px]:tracking-[0.015em]">
-                        <span className="text-white truncate">Search</span>
-                      </button>
-                    </div>
-                  </div>
-                </label>
-              </div>
-            </div>
-          </div>
-        )}
+        <h1 className="text-2xl text-right">Filtro de buscas brevemente...</h1>
+
         <h1 className=" text-[22px] font-bold leading-tight tracking-[-0.015em] px-4 text-left pb-3 pt-5">
-          Featured Articles
+          Ultimas publicações
         </h1>
 
         {last && (
@@ -138,7 +87,7 @@ export default function Blog() {
                 <Image
                   src={last.imageUrl}
                   alt="Post Image"
-                  className="w-screen rounded-xl desktop:w-auto h-[10rem] laptop:h-auto laptop:w-96 object-cover"
+                  className="w-screen rounded-xl desktop:w-auto h-[10rem] laptop:w-96 object-cover"
                   width={1000000}
                   height={1000000}
                 />
